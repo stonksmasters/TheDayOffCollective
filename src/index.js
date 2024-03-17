fcfef5f82fdfc87d02e2aa704a4c1b10c8e34ad6
stonkystonkster <john.borg237@gmail.com>
2024-03-16 19:13:46 -0500

    Updated README file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import ProductList from './components/ProductList';
+
 
 ReactDOM.render(
     <Router>
