755bc4fea2a8682a715a3cc9129124c197d86dd8
stonkystonkster <john.borg237@gmail.com>
2024-03-22 19:51:08 -0500

    shipping form

--- src/components/ShippingForm.js
+++ src/components/ShippingForm.js
@@ -16,24 +16,14 @@ const ShippingForm = () => {
         setFormData({ ...formData, [name]: value });
     };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log('Form Data:', formData);
-
-        // You can now submit the form data to your backend or to Netlify here
-    };
-
     return (
-        <form 
-        name="shipping" 
-        method="POST" 
-        data-netlify="true" 
-        onSubmit="submit">
+        <form name="shipping" method="POST" data-netlify="true">
+            <input type="hidden" name="form-name" value="shipping" />
             <p>
                 <label>Name: <input type="text" name="name" value={formData.name} onChange={handleChange} required /></label>
             </p>
             <p>
-                <label htmlFor= "email">Email (Optional): <input id="email" name="email" value={formData.email} onChange={handleChange} /></label>
+                <label>Email (Optional): <input type="email" name="email" value={formData.email} onChange={handleChange} /></label>
             </p>
             <p>
                 <label>Discord Username (Optional): <input type="text" name="discord" value={formData.discord} onChange={handleChange} /></label>
@@ -49,6 +39,7 @@ const ShippingForm = () => {
             </p>
             <p>
                 <label>Zip Code: <input type="text" name="zipcode" value={formData.zipcode} onChange={handleChange} required /></label>
+
             </p>
             <p>
                 <button type="submit">Proceed to Checkout</button>
