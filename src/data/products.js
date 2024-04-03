76a16b4afe629faa3b9b05637110cf0dca1c634a
stonksmasters <110305299+stonksmasters@users.noreply.github.com>
2024-04-03 14:49:02 -0500

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -4,7 +4,7 @@ const products = [
         id: '1',
         name: 'Drop 1 Products Pack',
         description: 'Featuring 1 Degen Toon Magnet, 1 Degen Toon Pin, 1 Degen Toon Holographic sticker, and 1 Goon Magnet.',
-        price: 0.035,
+        price: 0.16,
         images: [
             '/images/Holo.png',
             '/images/ToonMagnet.png',
